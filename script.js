e67174031f010bdd7ba3f2e4282bdf1365a8cf94
Hannah L. Hale <141271112+HannahLHale@users.noreply.github.com>
2024-10-24 14:00:22 -0400

    update

--- script.js
+++ script.js
@@ -15,13 +15,83 @@ $(document).ready(function () {
 
   // input text for typing animation
   $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
-/*
 
-3D images gallery. inspired from David DeSandro's tutorial
+var swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  initialSlide: 4,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  speed: 1000,
+  freeMode: false,
+  mousewheel: {
+    thresholdDelta: 30,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+});
 
-(http://24ways.org/2010/intro-to-css-3d-transforms)
-
-*/
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "circle",
+    },
+    opacity: {
+      value: 0.3,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 4,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 4,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 2,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 0.4,
+      direction: "right",
+      random: true,
+      straight: false,
+      out_mode: "none",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  retina_detect: true,
+});
   // initialize wow.js
   new WOW().init();
 
